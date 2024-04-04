918d73c8b3155c971c0cc2f65ec99c98cdebef85
minagishl <mvxd568.co@icloud.com>
2024-04-05 01:47:40 +0900

    The part that generates the secret is complete.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { customAlphabet } from 'nanoid';
 
-const app = new Hono()
+const app = new Hono();
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const nanoid = customAlphabet(characters, 10);
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+	return c.redirect('/32', 301);
+});
 
-export default app
+app.get('/:length', (c) => {
+	const length = Number(c.req.param('length'));
+	const secret = nanoid(length);
+	return c.text(secret);
+});
+
+export default app;
