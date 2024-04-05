961cf31310e33bde480e1147fdd5f3818282723e
minagishl <mvxd568.co@icloud.com>
2024-04-05 15:54:47 +0900

    Code Optimization

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 
 const app = new Hono();
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-const generateSecret = (length: number) =>
-	Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
 
 app.get('/', (c) => {
 	return c.redirect('/32', 301);
@@ -12,8 +9,17 @@ app.get('/', (c) => {
 
 app.get('/:length', (c) => {
 	const length = Number(c.req.param('length'));
-	const secret = generateSecret(length);
-	return c.text(secret);
+	if (!length) {
+		throw new HTTPException(401, { message: 'Invalid length.' });
+	}
+
+	const array = new Uint8Array(Math.ceil(length / 2));
+	crypto.getRandomValues(array);
+	const randomString = Array.from(array)
+		.map((b) => b.toString(16).padStart(2, '0'))
+		.join('')
+		.slice(0, length);
+	return c.text(randomString);
 });
 
 export default app;
